30c901a6c1be7c57e30fd540ff4bc76957a8b6aa
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-12-15 19:59:04 +0600

    [FEA-4]

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Bakai Terraform
+          Learn React with Bakai
         </a>
       </header>
     </div>
