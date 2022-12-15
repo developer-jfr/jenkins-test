d1dc59e4e27e7d7115028f9a5887a61745aff4bb
developer-jfr <85294383+developer-jfr@users.noreply.github.com>
2022-12-15 19:44:24 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Bakai
+          Learn React with Bakai Terraform
         </a>
       </header>
     </div>
