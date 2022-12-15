5f91e73c3c6d70abfea49043a016e1abac276ffc
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-12-15 19:40:02 +0600

    [FEA-2]

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Bakai
         </a>
       </header>
     </div>
